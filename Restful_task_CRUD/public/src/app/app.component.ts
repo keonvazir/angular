74b7d91ad0cb6b59f8062817c554da127594a108
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-12 13:41:51 -0800

    show all

---
+++ Restful_task_CRUD/public/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'public';
+  tasks;
+  task = "";
+  newTask: any;
+  editTask: any;
+  editTog: boolean = false;
+
+  constructor(private _httpService: HttpService){
+
+  }
+  ngOnInit(){
+    // this.getTasksFromService()
+    this.newTask = {title: "", description: ""}
+  }
+
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data =>{
+      console.log("~*******************************************~", data);
+      this.tasks = data
+    });
+  }
+  onSubmit() {
+    let observable = this._httpService.addTask(this.newTask);
+    observable.subscribe(data =>{
+      console.log("~Create~");
+      this.newTask = { title: "", description: "" }
+      this.getTasksFromService();
+    })
+  }
+  editForm(task){
+    this.editTask = {_id: task._id, title: task.title, description: task.description}
+  }
+  onEdit(){
+    let observable = this._httpService.editTask(this.editTask);
+    observable.subscribe(data => {
+      console.log("~Edit~");
+      this.editTog = false;
+      this.getTasksFromService();
+    })
+  }
+  onDelete(task){
+    let observable = this._httpService.deleteTask(task);
+    observable.subscribe(data => {
+      console.log("~Delete~");
+      this.getTasksFromService();
+    })
+  }
+}
---
+++ Restful_task_CRUD/public/src/app/http.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient) { }
+
+  getTasks(){
+    return this._http.get("/tasks");
+  }
+
+  getTasksById(_id:string){
+    return this._http.get("/tasks/:_id");
+  }
+
+  addTask(newtask){
+    return this._http.post("/tasks", newtask)
+  }
+
+  editTask(editTask){
+    return this._http.put("/tasks/:_id", editTask);
+  }
+
+  deleteTask(task){
+    return this._http.delete("/tasks/:_id", task);
+  }
+
+}
