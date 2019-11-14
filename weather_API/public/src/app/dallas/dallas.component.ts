db3e92f7df90d141fa3ba38a9c4ecfbeccae49b3
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-14 13:34:15 -0800

    dallas

--- weather_API/public/src/app/dallas/dallas.component.ts
+++ weather_API/public/src/app/dallas/dallas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../http.service';
 
 @Component({
   selector: 'app-dallas',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dallas.component.css']
 })
 export class DallasComponent implements OnInit {
+  weather: any;
+  temp: any;
+  maxTemp: any;
+  minTemp: any;
+  humidity: any;
+  wind: any;
+  clouds: any;
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
+    this.getDallasWeather();
+  }
+  getDallasWeather(){
+    this._httpService.getWeather("Dallas")
+    .then(data =>{
+      this.weather = data;
+      console.log(data)
+    })
+    .catch(err=>console.log(err))
+  }
   }
 
-}
