4c73d3e611b06500955ce77079608f07376e47d2
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-14 13:39:12 -0800

    dc

--- weather_API/public/src/app/dc/dc.component.ts
+++ weather_API/public/src/app/dc/dc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../http.service';
 
 @Component({
   selector: 'app-dc',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dc.component.css']
 })
 export class DcComponent implements OnInit {
+  weather: any;
+  temp: any;
+  maxTemp: any;
+  minTemp: any;
+  humidity: any;
+  wind: any;
+  clouds: any;
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
+    this.getDcWeather();
+  }
+  getDcWeather(){
+    this._httpService.getWeather("San Francisco")
+    .then(data =>{
+      this.weather = data;
+      console.log(data)
+    })
+    .catch(err=>console.log(err))
+  }
   }
 
-}
