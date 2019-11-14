8499c3adbd5722fd406bbcb37743c5aaff2f2714
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-14 13:30:41 -0800

    chicago

--- weather_API/public/src/app/chicago/chicago.component.ts
+++ weather_API/public/src/app/chicago/chicago.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../http.service';
 
 @Component({
   selector: 'app-chicago',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chicago.component.css']
 })
 export class ChicagoComponent implements OnInit {
+  weather: any;
+  temp: any;
+  maxTemp: any;
+  minTemp: any;
+  humidity: any;
+  wind: any;
+  clouds: any;
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
+    this.getChicagoWeather();
   }
-
-}
+  getChicagoWeather(){
+    this._httpService.getWeather("Chicago")
+    .then(data =>{
+      this.weather = data;
+      console.log(data)
+    })
+    .catch(err=>console.log(err))
+  }
+  }
