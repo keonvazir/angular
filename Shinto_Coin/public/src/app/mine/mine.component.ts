1dd13299c92749562c0573ae3c783e3f4bff2025
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-14 15:28:46 -0800

    not complete

--- Shinto_Coin/public/src/app/mine/mine.component.ts
+++ Shinto_Coin/public/src/app/mine/mine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-mine',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mine.component.css']
 })
 export class MineComponent implements OnInit {
+  trivia: any;
+  answers = [];
+  result: string;
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
+  
 
   ngOnInit() {
+    this.loadTrivia();
   }
 
+loadTrivia(){
+  let observable = this._httpService.getTrivia()
+  observable.subscribe(data => {
+    this.answers = [];
+    this.trivia = data;
+    this.answers.push(this.trivia.results[0].correct_answer);
+    for(var idx of this.trivia.results[0].incorrect_answer){
+      this.answers.push(idx)
+    }
+    this.shuffle(this.answers);
+  })
+}
+shuffle(a){
+  for(let i = a.length - 1; i>0; i--){
+    const j = Math.floor(Math.random()* (i+1));
+    [a[i], a[j]]=[a[j], a[i]];
+  }
+  return a;
+}
+//submit for the form
+
+triviaAnswer(answer){
+  if(answer == this.trivia.results[0].correct_answer){
+    this._httpService.yourCoins += 1;
+    this._httpService.coinValue += 1;
+    this._httpService.coinTrans('Mined', 1)
+    this.result = "Answer was correct! 1 ShintoCoin mined!"
+    this.loadTrivia();
+  }
+  else{
+    this.result="Answer was incorrect!"
+    this.loadTrivia();
+  }
+}
 }
