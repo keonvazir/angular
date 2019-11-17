1118d4a9c23c09299c562bf7835e058c3e500a86
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 12:03:53 -0800

    service

--- task-api-angular/public/src/app/http.service.ts
+++ task-api-angular/public/src/app/http.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+  getTasks(){
+    return this._http.get('/tasks')
+  }
+  getTaskById(id){
+    return this._http.get(`/tasks/${id}`);
+  }
+  createTask(task){
+    return this._http.post('/tasks/', task);
+  }
+  deleteTask(id){
+    return this._http.delete(`/tasks/${id}`);
+  }
+  updateTask(task){
+    return this._http.put(`/tasks/${task._id}`, task)
+  }
 }
