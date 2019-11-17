1dbf433594697dee5fbee8337e4816447b906ac3
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 13:11:05 -0800

    groupchat

---
+++ groupchat/public/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import io from 'socket.io-client';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
