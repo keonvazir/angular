5dc6a2eff13d148178a5add7eef00834cd795e91
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 17:21:36 -0800

    module

--- exam-prep/public/src/app/app.module.ts
+++ exam-prep/public/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpService } from './http.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { NewRevComponent } from './new-rev/new-rev.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { EditResComponent } from './edit-res/edit-res.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,13 +21,15 @@ import { EditResComponent } from './edit-res/edit-res.component';
     NewRevComponent,
     ReviewsComponent,
     RestaurantsComponent,
-    EditResComponent
+    EditResComponent,
+    HttpClientModule,
+    FormsModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
