99ed28663e967757730733c0f1dc80e9c25b896a
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 17:47:36 -0800

    restaurants

--- exam-prep/public/src/app/home/home.component.ts
+++ exam-prep/public/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
   }
+
 
 }
--- exam-prep/public/src/app/restaurants/restaurants.component.ts
+++ exam-prep/public/src/app/restaurants/restaurants.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from '../http.service';
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
-
-  constructor() { }
+  now: any;
+  restaurants: any;
+  constructor(private _httpService: HttpService, ) { }
 
   ngOnInit() {
+    this.getRestaurants();
+    console.log(this.now);
+    this.now = new Date();
+    console.log(this.now)
+  }
+
+  getRestaurants() {
+    this._httpService.getAllRestaurants()
+      .then(data => {
+        this.restaurants = data['data'];
+      })
+      .catch(err => console.log(err))
+  }
+
+  deleteRestaurant(id) {
+    this._httpService.deleteRestaurantById(id)
+      .then(() => this.getRestaurants())
+      .catch(err => console.log(err))
+  }
+
+  convertDate(date) {
+    return new Date(date);
   }
 
 }
