6971a131ddbc1d52d1bb7f9761f5516f3e22f23a
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 17:19:46 -0800

    service

--- exam-prep/public/src/app/http.service.ts
+++ exam-prep/public/src/app/http.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  getAllRestaurants(){
+    return this._http.get('/restaurants').toPromise();
+  }
+
+  createRestaurant(data){
+    return this._http.post('/restaurant', data).toPromise();
+  }
+
+  getByIdRestaurant(id){
+    return this._http.get(`/restaurants/${id}`).toPromise();
+  }
+
+  updateRestaurant(data){
+    return this._http.put(`/restaurants/${data._id}`, data).toPromise();
+  }
+
+  createReview(data, res_id){
+    return this._http.post(`/review/${res_id}`, data).toPromise();
+  }
+
+  deleteRestaurantById(id){
+    return this._http.delete(`/restaurants/${id}`).toPromise();
+  }
 }
