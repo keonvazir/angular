56c5bb6e2e0eb7d8fa6fa851c1e7fb23ad9dbf50
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 17:25:33 -0800

    approuting

--- exam-prep/public/src/app/app-routing.module.ts
+++ exam-prep/public/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewResComponent } from './new-res/new-res.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { EditResComponent } from './edit-res/edit-res.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { NewRevComponent } from './new-rev/new-rev.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/restaurantz'},
+  {path: 'restaurantz', component: RestaurantsComponent, children:[
+    {path: ':id/edit', component: EditResComponent}
+  ]},
+  {path: 'restaurantz/new', component: NewResComponent},
+  {path: 'restaurantz/:id', component: ReviewsComponent},
+  {path: 'restaurantz/:id/review', component: NewRevComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
