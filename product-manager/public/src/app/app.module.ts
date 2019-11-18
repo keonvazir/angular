ff479fdbd2f8f70c622f2f14329db0651a1c067d
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-17 18:11:26 -0800

    initial

---
+++ product-manager/public/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { EditComponent } from './edit/edit.component';
+import { CreateComponent } from './create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    HomeComponent,
+    EditComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
